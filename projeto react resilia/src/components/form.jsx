f94faaf1ef460dba93857183dc7c1c288ecab172
Vitor <v.augusto.am@gmail.com>
2022-08-18 19:53:01 -0300

    att

---
+++ projeto react resilia/src/components/form.jsx
@@ -0,0 +1,7 @@
+export const Form = () => {
+    return <form>
+        <input type="text" /><input type="email" /><input type="date" />
+        <textarea></textarea>
+        <button>Enviar</button>
+    </form>
+}
